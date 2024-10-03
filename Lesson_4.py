9217b441b6fac1bb5a18b71de4deae67f75a58de
Jumanji503 <jjorgen4me@gmail.com>
2024-10-03 15:02:27 +0100

    my first commit

---
+++ Lesson_4.py
@@ -0,0 +1,8 @@
+def John_func():
+    Name = "Jurgen John"
+    height = "182cm"
+    age = 21
+    Is_male = True
+    print(Name, height, age, Is_male)
+    
+John_func()
---
+++ Name.py
@@ -0,0 +1,1 @@
+Name = "Jurgen John"
